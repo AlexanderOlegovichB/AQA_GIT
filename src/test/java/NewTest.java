65a796c13bc2785f7028e6b63afab161fc7d8ca2
AlexanderQA <ip.boroday@gmail.com>
2025-04-02 15:58:00 +0300

    Added var

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 public class NewTest {
 
     @Test
-    void newTest() {}
-    Assertions.assertTrue(3 > 2);
+    void newTest() {
+        int i = 3;
+        Assertions.assertTrue(i > 2);
+    }
 }
